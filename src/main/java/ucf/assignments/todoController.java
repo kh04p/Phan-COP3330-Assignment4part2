9238578e5efda86dd64138fdcecb32b445579c9f
Khoa <khoa150899@gmail.com>
2021-07-01 22:35:33 -0400

    3rd Commit

--- src/main/java/ucf/assignments/todoController.java
+++ src/main/java/ucf/assignments/todoController.java
@@ -1,11 +1,25 @@
 package ucf.assignments;
 
-public class todoController {
-    public void button1() {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
+import java.awt.*;
+
+public class todoController {
+    @FXML
+    private TextField outputDisplay;
+
+    public void button1(ActionEvent actionEvent) {
+        display("Why did you click this??");
     }
 
-    public void button2() {
+    public void button2(ActionEvent actionEvent) {
+        display("Thanks.");
+    }
 
+    public void display(String text) {
+        outputDisplay.setText(text);
     }
 }
