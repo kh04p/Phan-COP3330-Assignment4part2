5e4baabba013b795641bf24bf8f5b07c4e765cac
Khoa <khoa150899@gmail.com>
2021-07-04 15:09:52 -0400

    5th Commit

--- src/main/java/ucf/assignments/todoController.java
+++ src/main/java/ucf/assignments/todoController.java
@@ -7,6 +7,50 @@ import javafx.scene.control.TextField;
 
 import java.awt.*;
 
+/**
+ * Sample Skeleton for 'todo.fxml' Controller Class
+ */
+
 public class todoController {
+
+    @FXML
+    void editDate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editDescription(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exportFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exportTodo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void markCompleted(ActionEvent event) {
+
+    }
+
+    @FXML
+    void markIncomplete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void quit(ActionEvent event) {
+
+    }
 
 }
